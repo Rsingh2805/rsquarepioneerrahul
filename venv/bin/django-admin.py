dbd5f78f292ff86459e9d84d2b5ea05227d7f702
Rsingh2805 <rs280599@gmail.com>
2019-12-14 21:27:26 +0530

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rahul/WORK/Python/Interns/rsquare-intern/Pioneer-master/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
